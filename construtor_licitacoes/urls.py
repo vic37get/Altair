364b3b2c3315c497f8af9dc0288b76c76d292201
marcelo janio <marcelostgm@gmail.com>
2022-08-09 17:39:11 -0300

    Adição de um campo de texto para o cabeçalho

--- construtor_licitacoes/urls.py
+++ construtor_licitacoes/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name='construtor_licitacoes'
 urlpatterns = [
     path('licitacao/',views.nova_licitacao,name='adicionar'),
-    path('licitacao/<pk>/editar/',views.editar_licitacao,name='editar'),
-    path('delete/<pk>', views.lic_delete, name='delete'),
+    #path('licitacao/<pk>/editar/',views.editar_licitacao,name='editar'),
+    #path('delete/<pk>', views.lic_delete, name='delete'),
 ]
