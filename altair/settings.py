7b0e5de5e3f518dca130ebb328c83ccc7f004cf3
Victor Ribeiro da Silva <victorribeiro172@gmail.com>
2022-08-29 08:47:52 -0300

    #38 adição do workflow

--- altair/settings.py
+++ altair/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-ja$vi6&d99ou7ijvcdea6_3yf05n3#v&(p44k9unw%&%z#^bkq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'altairlic.herokuapp.com']
 
 # Application definition
 
@@ -131,9 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
---
+++ staticfiles/construtor_licitacoes/js/downloadPdf.js
@@ -0,0 +1,51 @@
+function baixarPdf(salvar){
+    var ids = getAllIds();
+    var divconteudo = []
+    var divs_secoes = document.getElementsByClassName('conteudoCaptura');
+    var textoInicial = tinymce.get('cabecalho').getContent();
+    if(divs_secoes.length>0)
+      divs_secoes[0].childNodes[0]
+
+    for (let index = 0; index < divs_secoes.length; index++) {
+      var div_conteudo = document.createElement('div');
+      div_conteudo.setAttribute('class','w-100')
+      var filhos = divs_secoes[index].childNodes;
+      if(filhos[0].tagName == 'DIV'){
+        var titulo = tinymce.get(filhos[0].childNodes[1].id).getContent();
+        div_conteudo.innerHTML = titulo;
+      }
+
+      for (let j = 1; j < filhos.length; j++) {
+        var elemento = filhos[j];
+        if(elemento.id.match(/mce_.*/i)!=null){
+          var conteudo = tinymce.get(elemento.id).getContent();
+          div_conteudo.innerHTML = conteudo;
+          divconteudo.push((divs_secoes[index].childNodes[0]).outerHTML)
+          divconteudo.push(conteudo)
+        }
+       
+      }
+    }
+    var stringpdf = "<h4>Preambulo</h4>"+textoInicial;
+    divconteudo.forEach(element => {
+      stringpdf += element
+    });
+    //console.log(stringpdf)
+
+    var doc = new jsPDF();
+    var data = new Date();
+    
+    var dataAtual = new Date();
+    var dataAtual = dataAtual.getDate()+'-'+(dataAtual.getMonth()+1)+'-'+dataAtual.getFullYear();
+
+    doc.fromHTML(stringpdf, // page element which you want to print as PDF
+    15,
+    15, 
+    {
+      'width': 170
+    },
+    function(a) 
+    {
+        doc.save("edital "+ dataAtual.toString()+".pdf","../");
+    });
+  }
