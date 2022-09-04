fea4df9fe7328b7ff9fa4e5ff3f256f59ed71fe8
Victor <victorribeiro172@gmail.com>
2022-09-04 16:25:40 -0300

    #43 remove forms

--- construtor_licitacoes/models.py
+++ construtor_licitacoes/models.py
@@ -1,33 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class LicitacaoForm(models.Model):
-
-    orgao = models.CharField(
-        max_length=255,
-        null=False,
-        blank=False
-  )
-
-    municipio = models.CharField(
-        max_length=255,
-        null=False,
-        blank=False
-  )
-
-    tipoObjeto = models.CharField(
-        max_length=25,
-        null=False,
-        blank=False
-  )
-
-    objeto = models.CharField(
-        max_length=255,
-        null=False,
-        blank=False
-  )
-
-    dataAbertura = models.DateField(
-        null=False,
-        blank=False
-  )
