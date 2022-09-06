6a07f473193e61b7d15de481385d90a39ea84163
Jasson Silva <jassonjcs11@gmail.com>
2022-09-06 09:03:06 -0300

    Removido teste que não deveria estar lá #48

--- construtor_licitacoes/tests/test_pdf.py
+++
@@ -1,8 +0,0 @@
-from django.test import SimpleTestCase
-import weasyprint
-import os
-
-class PdfTest(SimpleTestCase):
-    def test_pdf_python(self):
-        pdf = weasyprint.HTML(filename=os.path.join(os.getcwd(),'construtor_licitacoes','tests','sample.html')).write_pdf()
-        open(os.path.join(os.getcwd(),'construtor_licitacoes','tests','google.pdf'), 'wb').write(pdf)
