824625af2983f91861874c2f8f96f59b36a7fed1
marceloJanio <marcelostgm@gmail.com>
2022-09-07 17:23:46 -0300

    show pdf avaliar #50

--- verificador_fraude/urls.py
+++ verificador_fraude/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name='verificador_fraude'
 urlpatterns = [
-    path('aud', views.homeAud, name='homeAud')
+    path('aud', views.homeAud, name='homeAud'),
+    path('aud/avaliar/<pk>', views.avaliar, name='avaliar')
 ]
