26e5d87a73f57fa03980416c88c22b3a404c8560
Jasson Silva <jassonjcs11@gmail.com>
2022-09-10 15:30:31 -0300

    #49 Desabilitada edição de submetidas

--- construtor_licitacoes/static/construtor_licitacoes/js/index.js
+++ construtor_licitacoes/static/construtor_licitacoes/js/index.js
@@ -1,22 +1,23 @@
 
 function statusLicitacao(licitacao,id) {
-
-    console.log("isso aí mermo: "+licitacao);
     if(licitacao==0){
         document.getElementById("status"+id).setAttribute('class','status pendente fa-solid fa-circle');
         document.getElementById("Labelstatus"+id).innerHTML = "Pendente";
-        console.log("status"+id)
-
     }
 
     if(licitacao==1){
         document.getElementById("Labelstatus"+id).innerHTML = "Submetido";
         document.getElementById("status"+id).setAttribute('class','status submetido fa-solid fa-circle');
-        document.getElementById("editar"+id).setAttribute('visibility','hidden');
-        
-        console.log("status"+id)
-
     }
+  }
 
-
+  function switchLockEdit(licitacao,id) {
+    if(licitacao==0){
+        document.getElementById("editar"+id).disabled = false;
+        document.getElementById("excluir"+id).disabled = false;
+    }
+    if(licitacao!=0){
+        document.getElementById("editar"+id).disabled = true;
+        document.getElementById("excluir"+id).disabled = true;
+    }
   }
