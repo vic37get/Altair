73ce49c135f586adefc536e2d6ca9866669bcdcf
marceloJanio <marcelostgm@gmail.com>
2022-09-13 08:58:30 -0300

    criacao tokeniza and methods #69

---
+++ logic/Tokeniza.py
@@ -0,0 +1,31 @@
+import Header
+from logic import Fraude
+from LicitacaoProxy import ProxyLicitacao
+from logic import RegraProxy
+from logic import ProxyDataManipulation
+
+class Main:
+    def _init_(self,source=Header.SOURCE_r):
+        self.all()
+
+    def all(self):
+        dataManipulation = ProxyDataManipulation()
+        dataManipulation.createEmptyFiles()
+        dataManipulation.createEmptydataFrame(Header.OUTPUT_NOME)
+        nomes = dataManipulation.getNomesEditais(Header.SOURCE_r)
+        lista_fraudes = []
+        for count,nome in enumerate(nomes):
+            #if count > 1000:
+                #break
+            dados = dataManipulation.readTxTFiles(nome,Header.SOURCE_r)
+            ProxyLic = ProxyLicitacao('',nome,dados)
+            ProxyLic.struct()
+            #----- Salva no txts -----------
+            dataManipulation.saveTypeFiles(ProxyLic)
+            for i in ProxyLic.getSecao():
+                pass
+            if ProxyLic.getTipoValidade() == Header.TIPOS['VALIDO']:
+                verificadorFraude = Fraude(ProxyLic)
+                verificadorFraude.getAchados()
+                lista_fraudes.append(verificadorFraude)
+        dataManipulation.saveResultsAll(lista_fraudes)
