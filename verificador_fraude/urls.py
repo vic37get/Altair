d29b13f926382dbb98294a10de0c427ac89694ed
Jasson Silva <jassonjcs11@gmail.com>
2022-09-13 20:39:38 -0300

    Verificando indicios com sucesso #84

--- verificador_fraude/urls.py
+++ verificador_fraude/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('aud', views.homeAud, name='homeAud'),
     path('aud/avaliar/<pk>', views.avaliar, name='avaliar'),
     path('filtroVerificador',views.filtroVerificador,name='filtroVerificador'),
+    path('aud/avaliar/<pk>/verificar',views.verificar,name='verificar'),
 ]
