e7d28faa3dd17bd6b683998680af62abf8fe08bb
marceloJanio <marcelostgm@gmail.com>
2022-09-12 21:29:02 -0300

    verifica fraude completa #68

--- logic/Fraude.py
+++ logic/Fraude.py
@@ -5,4 +5,16 @@ class Fraude:
         self._Licitacao = Licitacao
         self.achados = None
     
+    def getLicitacao(self):
+        return self._Licitacao
+
+    def verificaFraudeCompleta(self,expressao,tipo):
+        for j in self._Licitacao.getSecao():
+            if j.getTipo() == tipo:
+                achado = expressao.search(j.getConteudo())
+                if achado[1]:
+                    achado_obj = Achado(expressao.getNome(),j.getTitulo(),'','')
+                    achado_obj.setConteudoAchado(j.getConteudo())
+                    return achado_obj
+        return Achado(expressao.getNome(),None,'','')
     
