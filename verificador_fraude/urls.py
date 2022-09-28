08c8353ab1c45510a84929f2ff154037926e2a4b
Victor Ribeiro da Silva <victorribeiro172@gmail.com>
2022-09-28 09:31:35 -0300

    #96 conserto do html

--- verificador_fraude/urls.py
+++ verificador_fraude/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name='verificador_fraude'
 urlpatterns = [
-    path('aud', views.homeAud, name='homeAud'),
+    path('', views.homeAud, name='homeAud'),
     path('aud/avaliar/<pk>', views.avaliar, name='avaliar'),
     path('filtroVerificador',views.filtroVerificador,name='filtroVerificador'),
     path('aud/avaliar/<pk>/verificar',views.verificar,name='verificar'),
