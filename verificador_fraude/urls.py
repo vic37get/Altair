18714b60e4f2c031c64560e0c9f1a800a5a59a1f
Jasson Silva <jassonjcs11@gmail.com>
2022-10-01 17:45:25 -0300

    logica de avaliação realizada #93

--- verificador_fraude/urls.py
+++ verificador_fraude/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('aud/avaliar/<pk>', views.avaliar, name='avaliar'),
     path('filtroVerificador',views.filtroVerificador,name='filtroVerificador'),
     path('avaliar/<pk>/verificar',views.verificar,name='verificar'),
+    path('avaliacao/<pk>',views.avalicao,name='avaliacao')
 ]
